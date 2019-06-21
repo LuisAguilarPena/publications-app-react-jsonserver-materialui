9fc31efd386dfeb824186df0e2408c8a1c164e59
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 14:07:53 -0500

    refactor to only have the layout component instead of body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-import Body from './containers/Body/Body';
 
 class App extends Component{
   render(){
     return(
       <div>
-        <Layout>
-          <Body />
-        </Layout>
+        <Layout />
       </div>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,25 @@ import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
+import axios from '../../axiosreq';
+import Card from '../Card/Card';
 
 class Layout extends Component {
   state = {
     showSideDrawer: false,
-    
+    "articles": null,
+    "error": false
   }
   
+  componentDidMount () {
+    axios.get('http://localhost:3001/articles?_page=1$_limit=10')
+      .then(response => {
+        console.log(response.data);
+        this.setState({articles: response.data});
+      })
+      .catch(error => {this.setState({error: true})});
+  } 
+
   sideDrawerClosedHandler = () => {
     this.setState({showSideDrawer: false});
   }
@@ -21,6 +33,26 @@ class Layout extends Component {
   } 
 
   render () {
+
+    let card = this.state.error ? <Aux><p>Game Over</p></Aux> : <Aux><p>Loading</p></Aux>;
+  
+    if(this.state.articles){
+      card = (
+        <Aux>
+          {this.state.articles.map(article => 
+            <Card 
+              key={article.id}
+              tittle={article.tittle}
+              author={article.author}
+              body={article.body}
+              description={article.description}
+              date={article.date}
+            />
+          )}
+        </Aux>    
+      )
+    }
+
     return (
       <Aux>
         <div>
@@ -28,7 +60,7 @@ class Layout extends Component {
           <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
         </div>
         <main className={classes.Content}>
-          {this.props.children}
+          {card}
         </main>
       </Aux> 
     )
