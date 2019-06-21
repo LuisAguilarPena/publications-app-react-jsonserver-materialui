cd4ab5a4076b16cba216d60397a0c43b6f2426a5
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 13:46:22 -0500

    rendering authors dynamically on the sideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
+import axios from '../../../axiosreq';
 
-const sideDrawer = props => {
-  let attachedClasses = [classes.SideDrawer, classes.Close]
-  if(props.open) {
-    attachedClasses = [classes.SideDrawer, classes.Open];
+class SideDrawer extends Component {
+  state = {
+    "authors": null,
+    "error": false
   }
-  return (
-    <Aux>
-      <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={attachedClasses.join(' ')}>
-        <h3 className={classes.Author}>Albert Einstein</h3>
-        <h3 className={classes.Author}>Johann Gauss</h3>
-        <h3 className={classes.Author}>Ada Lovelace</h3>
-      </div>
-    </Aux>  
-  )
+
+  componentDidMount () {
+    axios.get('http://localhost:3001/authors')
+      .then(response => {
+        console.log(response.data);
+        this.setState({authors: response.data});
+      })
+      .catch(error => {this.setState({error: true})});
+  } 
+
+  render () {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if(this.props.open) {
+      attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    let author = this.state.error ? <Aux><p>Game Over</p></Aux> : <Aux><p>Loading</p></Aux>;
+  
+    if(this.state.authors){
+      author = (
+        <Aux>
+          {this.state.authors.map(author => 
+          <h3 
+            className={classes.Author}
+            key={author.id}
+          >
+            {`${author.firstName} ${author.lastName}`}
+          </h3>
+        )}
+        </Aux>    
+      )
+    }
+
+    return (
+      <Aux>
+        <Backdrop show={this.props.open} clicked={this.props.closed}/>
+        <div className={attachedClasses.join(' ')}>
+          {author}
+        </div>
+      </Aux>  
+    )
+  }
 }
 
-export default sideDrawer;
+export default SideDrawer;
