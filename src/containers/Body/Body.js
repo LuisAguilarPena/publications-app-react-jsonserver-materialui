31b6fc80413de1409bcac8ff41b7db9e6746748c
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 01:37:55 -0500

    added a body placeholder component where we will display publications as card with the required info

---
+++ src/containers/Body/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component{
+  render(){
+    return(
+      <div>
+        Card
+      </div>
+    );
+  }
+}
+
+export default Body;
