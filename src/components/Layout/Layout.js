188f76387fee1718c52dfb40bb326fcca1725d65
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 10:49:22 -0500

    added sidedrawer placeholder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
 const layout = props => (
   <Aux>
     <div>
       <Toolbar />
+      <SideDrawer />
     </div>
     <main className={classes.Content}>
       {props.children}
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { classes } from 'istanbul-lib-coverage';
+
+const sideDrawer = props => {
+  return (
+    <div className={classes.SideDrawer}>
+      <h3>Author</h3>
+      <h3>Authorx</h3>
+      <h3>Authorxx</h3>
+    </div>
+  )
+}
+
+export default sideDrawer;
