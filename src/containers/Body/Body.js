79daafc62f168ca59f7c90040210d8248b27e96a
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 12:45:33 -0500

    get request to set articles from fake api as state in Body, only first 10

--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -1,110 +1,13 @@
 import React, { Component } from 'react';
+import axios from '../../axiosreq';
+import Aux from '../../hoc/Aux';
 import Card from '../../components/Card/Card';
+
 
 class Body extends Component{
 
   state = {
-    "articles": [
-      {
-        "id": "1",
-        "tittle": "Tittle example 1",
-        "description": "Physics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-05-25",
-        "authorId": "1",
-        "author": "Albert Einstein"
-      },
-      {
-        "id": "2",
-        "tittle": "Tittle example 2",
-        "description": "Physics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-18",
-        "authorId": "1",
-        "author": "Albert Einstein"
-      },
-      {
-        "id": "3",
-        "tittle": "Tittle example 3",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-18",
-        "authorId": "2",
-        "author": "Johann Gauss"
-      },
-      {
-        "id": "4",
-        "tittle": "Tittle example 4",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-18",
-        "authorId": "2",
-        "author": "Johann Gauss"
-      },
-      {
-        "id": "5",
-        "tittle": "Tittle example 5",
-        "description": "Physics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-19",
-        "authorId": "1",
-        "author": "Albert Einstein"
-      },
-      {
-        "id": "6",
-        "tittle": "Tittle example 6",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-19",
-        "authorId": "3",
-        "author": "Ada Lovelace"
-      },
-      {
-        "id": "7",
-        "tittle": "Tittle example 7",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-19",
-        "authorId": "3",
-        "author": "Ada Lovelace"
-      },
-      {
-        "id": "8",
-        "tittle": "Tittle example 8",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-19",
-        "authorId": "2",
-        "author": "Johann Gauss"
-      },
-      {
-        "id": "9",
-        "tittle": "Tittle example 9",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-20",
-        "authorId": "2",
-        "author": "Johann Gauss"
-      },
-      {
-        "id": "10",
-        "tittle": "Tittle example 10",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-20",
-        "authorId": "2",
-        "author": "Johann Gauss"
-      },
-      {
-        "id": "11",
-        "tittle": "Tittle example 11",
-        "description": "Mathematics",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.",
-        "date": "2019-06-20",
-        "authorId": "3",
-        "author": "Ada Lovelace"
-      }
-    ],
+    "articles": null,
     "authors": [
       {
         "id": "1",
@@ -124,22 +27,43 @@ class Body extends Component{
         "lastName": "Lovelace",
         "email": "ada@gmail.com"
       }
-    ]
+    ],
+    "error": false
   }
 
+  componentDidMount () {
+    axios.get('http://localhost:3001/articles?_page=1$_limit=10')
+      .then(response => {
+        console.log(response.data);
+        this.setState({articles: response.data});
+      })
+      .catch(error => {this.setState({error: true})});
+  } 
+
   render(){
+    
+    let card = this.state.error ? <Aux><p>Game Over</p></Aux> : <Aux><p>Loading</p></Aux>;
+  
+    if(this.state.articles){
+      card = (
+        <Aux>
+          {this.state.articles.map(article => 
+            <Card 
+              key={article.id}
+              tittle={article.tittle}
+              author={article.author}
+              body={article.body}
+              description={article.description}
+              date={article.date}
+            />
+          )}
+        </Aux>    
+      )
+    }
+
     return(
       <div>
-        {this.state.articles.map(article => 
-          <Card 
-            key={article.id}
-            tittle={article.tittle}
-            author={article.author}
-            body={article.body}
-            description={article.description}
-            date={article.date}
-          />
-        )}
+        {card}
       </div>
     );
   }
