528fb01d46c4de3f6d464d755eb07352c93febc5
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-21 02:14:28 -0500

    passing props to render card info

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import classes from './Card.module.css';
 
-const card = props => (
-  <div className={classes.Card}>
-    <h2>Tittle</h2>
-    <h3>Author Luis Aguilar</h3>
-    <p>Lorem ipsum...</p>
-    <p>Description</p>
-    <p>2019-06-21</p>
-  </div>
-);
+const card = props => {
+
+  const transformedAuthors = props.authors;
+  const transformedArticles = props.articles;
+  console.log(transformedArticles);
+  
+  return (
+    <div className={classes.Card}>
+      <h2>{transformedArticles[0].tittle}</h2>
+      <h3>{`${transformedAuthors[0].firstName} ${transformedAuthors[0].lastName}`}</h3>
+      <p>{transformedArticles[0].body}</p>
+      <p>{transformedArticles[0].description}</p>
+      <p>{transformedArticles[0].date}</p>
+    </div>
+  );
+};
 
 export default card;
